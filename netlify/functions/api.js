0ea30aa0cd5805a024ea95e823e101581756382b
hebaarafat159 <heba.arafat159@gmail.com>
2023-10-16 19:03:41 +0100

    try to fix netlify issue

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,15 +1,15 @@
-// import 'dotenv/config'
-// import express from "express";
-// import cors from "cors";
-// import bodyParser from "body-parser";
+import 'dotenv/config'
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
-// import categoryRouter from './routers/category.js'
-// import productRouter from './routers/product.js'
-// import userRouter from './routers/user.js'
-// import listRouter from './routers/shoppingList.js'
+import categoryRouter from './routers/category.js'
+import productRouter from './routers/product.js'
+import userRouter from './routers/user.js'
+import listRouter from './routers/shoppingList.js'
 
-// import express, { Router } from "express";
-// import serverless from "serverless-http";
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
 // const app = express();
 
@@ -41,9 +41,14 @@ import serverless from "serverless-http";
 
 const api = express();
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+// const router = Router();
+// router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
-
+// api.use("/api/", router);
+api.use(cors());
+api.use(bodyParser.json());
+api.use("/categories", categoryRouter);
+api.use("/products", productRouter);
+api.use("/users", userRouter);
+api.use("/shoppingLists",listRouter);
 export const handler = serverless(api);
