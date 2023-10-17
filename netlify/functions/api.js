9609721da0d73ba3bb15f1420eb625563cb56863
hebaarafat159 <heba.arafat159@gmail.com>
2023-10-17 15:21:00 +0100

    fix netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,15 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
+import categoryRouter from '../../routers/category.js'
+
+
 const api = express();
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
+app.use("/api/categories", categoryRouter);
 
 export const handler = serverless(api);
