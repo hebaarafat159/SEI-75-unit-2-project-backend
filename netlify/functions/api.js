c88811c0701b77a26990bae3a5f67fb57d3f2502
hebaarafat159 <heba.arafat159@gmail.com>
2023-10-17 16:12:16 +0100

    netlify add all routers

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,7 +2,9 @@ import express, { Router } from "express";
 import serverless from "serverless-http";
 
 import categoryRouter from '../../routers/category.js'
-
+import productRouter from '../../routers/product.js'
+import userRouter from '../../routers/user.js'
+import listRouter from '../../routers/shoppingList.js'
 
 const api = express();
 
@@ -11,5 +13,8 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 api.use("/api/categories", categoryRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/shoppingLists",listRouter);
 
 export const handler = serverless(api);
